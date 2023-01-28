d8ce7394909f61ca890ff516f23046218773255e
r5oc <ofcoliva@gamil.com>
2023-01-28 14:45:24 -0300

    Vector created

--- src/vector/tests/TestsGetSize.java
+++ src/vector/tests/TestsGetSize.java
@@ -6,6 +6,6 @@ public class TestsGetSize {
     public static void main(String[] args) {
         Vector vector = new Vector(4);
         System.out.println(vector.getSize());
-        System.out.println(vector);      
+        System.out.println(vector);
     }
 }
