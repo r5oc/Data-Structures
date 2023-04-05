9a74a68a34cb404e9d930382ce23e84f0e07987c
r5oc <ofcoliva@gmail.com>
2023-04-05 18:19:47 -0300

    remove hashcode and equals

--- src/list/tests/Car.java
+++ src/list/tests/Car.java
@@ -9,33 +9,6 @@ public class Car {
         this.year = year;
     }
 
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((name == null) ? 0 : name.hashCode());
-        result = prime * result + year;
-        return result;
-    }
-    
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Car other = (Car) obj;
-        if (name == null) {
-            if (other.name != null)
-                return false;
-        } else if (!name.equals(other.name))
-            return false;
-        if (year != other.year)
-            return false;
-        return true;
-    }
 
     @Override
     public String toString() {
